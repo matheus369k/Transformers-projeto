de6f377dfb66cbffc6fded24478eccf9a772f9e1
Matheus Gomes <47065962+matheus369k@users.noreply.github.com>
2023-11-21 14:30:34 -0300

    creater media query; auto slide... project finily..

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,142 @@
 const containes = document.querySelectorAll('.container_Information')
+
 const navlist = document.getElementById('container_navbar_list')
 
 navlist.childNodes.forEach((nav, index) => {
+
+    document.body.addEventListener("touchmove", ()=> {
+
+        switch (index) {
+
+            case 1:
+
+                var navtouch = navlist[1]
+
+                break
+
+            case 3:
+
+                var navtouch = navlist[3]
+
+                break
+
+            case 5:
+
+                var navtouch = navlist[5]
+
+                break
+        }
+
+        SwitchPage(index, navtouch)
+
+        return
+    })
+
+
     nav.addEventListener('click', () => {
 
-        console.log(navlist, index, containes);
+        SwitchPage(index, nav)
 
-        const ContainerActive = document.querySelector('.container_Active')
-        if (ContainerActive) {
-            ContainerActive.classList.remove('container_Active')
-            document.querySelector('.active').classList.remove('active')
-        }
-        switch (index) {
-            case 1:
-                var count = 0
-                break;
-            case 3:
-                var count = 1
-                break;
-            case 5:
-                var count = 2
-                break;
-            case 7:
-                var count = 3
-                break;
-        }
-        containes[count].classList.add('container_Active')
-        nav.classList.add('active')
+        return
+
     })
 })
 
+
+
+const FocusImg = document.getElementById("slide_img");
+
+
+
+setInterval(() => {
+
+    AutoSwitchImg();
+
+}, 8000);
+
+const slideli = document.querySelectorAll(".slide")
+
+
+slideli.forEach((img, index) => {
+
+    img.addEventListener("click", ()=>{
+
+        RemoveClassThumbSelected(img);
+
+        FocusImg.src=`./src/img/${Number(index) + 1}.jpeg`
+
+        return
+    })
+})
+
+function SwitchPage(index, nav) {
+
+    const ContainerActive = document.querySelector('.container_Active')
+
+    if (ContainerActive) {
+
+        ContainerActive.classList.remove('container_Active')
+
+        document.querySelector('.active').classList.remove('active')
+
+    }
+
+    switch (index) {
+
+        case 1:
+
+            var count = 0
+
+            break
+
+        case 3:
+
+            var count = 1
+
+            break
+
+        case 5:
+
+            var count = 2
+
+            break
+    }
+
+    containes[count].classList.add('container_Active')
+
+    nav.classList.add('active')
+
+
+    return
+}
+
+
+
+function AutoSwitchImg() {
+
+    const urlImg = FocusImg.attributes.src.value;
+
+    RemoveClassThumbSelected(slideli[urlImg[(urlImg.length) - 6]])
+
+    FocusImg.src = `./src/img/${Number(urlImg[(urlImg.length) - 6]) + 1}.jpeg`;
+
+
+    return 
+}
+
+
+
+function RemoveClassThumbSelected(img) {
+
+    const thumbselected = document.querySelector(".container_information_slide_list_img_selected");
+
+    if (thumbselected) {
+
+        thumbselected.classList.remove("container_information_slide_list_img_selected");
+
+    }
+
+    img.classList.add("container_information_slide_list_img_selected");
+
+    return
+}
